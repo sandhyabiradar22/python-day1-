192557dc114224e58799684e71c234b301771f96
sandhyabiradar22 <sandhyabiradar22@gmail.com>
2026-08-16 19:37:56 +0530

    change

---
+++ datatype.py
@@ -0,0 +1,14 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+age_text = int(age_text)
+course_fee_text = float(course_fee_text)
+attempts =  str(attempts)
+print(age_text)
+print(type(age_text))
+print(course_fee_text)
+print(type(course_fee_text))
+print(attempts)
+print(type(attempts))
+
+
