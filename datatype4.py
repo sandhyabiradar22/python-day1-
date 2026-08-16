7cd120decd1d036c4c14e6b0c7f66ec1c254a039
sandhyabiradar22 <sandhyabiradar22@gmail.com>
2026-08-16 20:16:49 +0530

    change

---
+++ datatype4.py
@@ -0,0 +1,4 @@
+first_number = int(input("Enter the first number:"))
+secound_number = int(input("Enter the secound number"))
+sum_resulte = first_number + secound_number
+print(f"Sum: {sum_resulte}")
